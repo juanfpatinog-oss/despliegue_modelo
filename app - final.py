26c5241eb6709f5604b18c42420130eb509d70be
juanfpatinog-oss <juanfpatinog@gmail.com>
2026-04-07 22:12:35 -0500

    Add files via upload

--- app - final.py
+++ app - final.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 #Cargamos el modelo
 import pickle
 filename = 'modelo-NN.pkl'
-modelo, labelencoder, min_max_scaler, variables = pickle.load(open(filename, 'rb'))
+modelo, labelencoder, variables, min_max_scaler = pickle.load(open(filename, 'rb'))
  
  
 #Se crea interfaz gráfica con streamlit para captura de los datos
@@ -16,7 +16,7 @@ Unidad_Medida = st.selectbox('Unidad Medida', ["'UN'","'ML'","'KG'","'LT'"])
 Numero_de_Transacciones = st.slider('Numero de Transacciones',min_value=1, max_value=150, value=0, step=1)
 Cantidad = st.slider('Cantidad',min_value=-30000, max_value=0, value=0, step=1)
 Reintegros = st.slider('Reintegro',min_value=0, max_value=2500, value=0, step=1)
-Precio_Unitario = st.slider('Precio_Unitario',min_value=1, max_value=300000000, value=0, step=100000)
+Precio_Unitario = st.slider('Precio_Unitario',min_value=1, max_value=30000000, value=0, step=100000)
 Entrega = st.selectbox('Entrega', ["'TOTAL'","'PARCIAL'","'REINTEGRO'"])
 
  
