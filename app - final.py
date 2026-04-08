c42f260a24635f04671bc9849014e349e2da9068
juanfpatinog-oss <juanfpatinog@gmail.com>
2026-04-07 22:19:22 -0500

    Add files via upload

--- app - final.py
+++ app - final.py
@@ -12,23 +12,23 @@ modelo, labelencoder, variables, min_max_scaler = pickle.load(open(filename, 'rb
 #Se crea interfaz gráfica con streamlit para captura de los datos
 import streamlit as st
 st.title('Predicción de cluster de un material nuevo')
-Unidad_Medida = st.selectbox('Unidad Medida', ["'UN'","'ML'","'KG'","'LT'"])
-Numero_de_Transacciones = st.slider('Numero de Transacciones',min_value=1, max_value=150, value=0, step=1)
+Unidad Medida = st.selectbox('Unidad Medida', ["'UN'","'ML'","'KG'","'LT'"])
+Numero de Transacciones = st.slider('Numero de Transacciones',min_value=1, max_value=150, value=0, step=1)
 Cantidad = st.slider('Cantidad',min_value=-30000, max_value=0, value=0, step=1)
 Reintegros = st.slider('Reintegro',min_value=0, max_value=2500, value=0, step=1)
-Precio_Unitario = st.slider('Precio_Unitario',min_value=1, max_value=30000000, value=0, step=100000)
+Precio Unitario = st.slider('Precio Unitario',min_value=1, max_value=30000000, value=0, step=100000)
 Entrega = st.selectbox('Entrega', ["'TOTAL'","'PARCIAL'","'REINTEGRO'"])
 
  
 #Dataframe
-datos = [[Unidad_Medida, Numero_de_Transacciones, Cantidad, Reintegros, Precio_Unitario, Entrega]]
-data = pd.DataFrame(datos, columns=['Unidad_Medida', 'Numero_de_Transacciones', 'Cantidad', 'Reintegros', 'Precio_Unitario', 'Entrega']) #Dataframe con los mismos nombres de variables
+datos = [[Unidad Medida, Numero de Transacciones, Cantidad, Reintegros, Precio Unitario, Entrega]]
+data = pd.DataFrame(datos, columns=['Unidad Medida', 'Numero de Transacciones', 'Cantidad', 'Reintegros', 'Precio Unitario', 'Entrega']) #Dataframe con los mismos nombres de variables
 
  
 #Se realiza la preparación
 data_preparada=data.copy()
 #En despliegue drop_first= False
-data_preparada = pd.get_dummies(data_preparada, columns=['Unidad_Medida','Entrega'], drop_first=False, dtype=int)
+data_preparada = pd.get_dummies(data_preparada, columns=['Unidad Medida','Entrega'], drop_first=False, dtype=int)
 data_preparada.head()
 #Se adicionan las columnas faltantes
 data_preparada = data_preparada.reindex(columns=variables, fill_value=0)
@@ -36,7 +36,7 @@ data_preparada = data_preparada.reindex(columns=variables, fill_value=0)
  
 #Se normaliza las variables númericas para la Red
 #En los despliegues no se llama fit
-variables_numericas = ['Numero_de_Transacciones', 'Cantidad', 'Reintegros', 'Precio_Unitario']
+variables_numericas = ['Numero de Transacciones', 'Cantidad', 'Reintegros', 'Precio Unitario']
 data_preparada[variables_numericas] = min_max_scaler.transform(data_preparada[variables_numericas])
  
 #Hacemos la predicción
